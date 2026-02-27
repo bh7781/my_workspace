b3a3f3d94668ca65639a772da60b0d00bda52cdf
Chinmay <chinmay.s077@gmail.com>
2026-02-27 11:53:26 +0530

    Day 2 - Practise - 100 days of Python Challenge

---
+++ practise/udemy/100_days_of_code/day2_project2_tip_calculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill?: $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15?: "))
+no_of_splits = int(input("How many people to split the bill?: "))
+print(f"Each person should pay: ${round(bill * (1 + (tip / 100)) / no_of_splits, 2)}")
